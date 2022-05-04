bbfae16ab5c2e00a7cfdad89abf8e04d0c5ea6fb
Meenakshi <win.meena@gmail.com>
2022-05-05 05:27:14 +0530

    js changes

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ const arrowDown = document.querySelectorAll('.icon-angle-down');
 const featuresList = document.querySelector('.header__features__list');
 const companyList = document.querySelector('.header__company__list');
 
+const chkTab = function (i) {
+    i === 0 ? featuresList.classList.toggle('invisible') :
+        companyList.classList.toggle('invisible');
+}
+
 const showDropDown = function (i) {
     console.log('Showing dropdown');
     arrowDown[i].classList.toggle('invisible');
@@ -13,22 +18,15 @@ const showDropDown = function (i) {
 
 const hideDropDown = function (i) {
     console.log('Hiding dropdown');
-    
     arrowDown[i].classList.toggle('invisible');
     arrowUp[i].classList.toggle('invisible');
 }
 
-console.log(arrowUp);
-const chkTab = function (i) {
-    i === 0 ? featuresList.classList.toggle('invisible') :
-        companyList.classList.toggle('invisible');
-}
 arrowUp.forEach((icon,i) => {
 icon.addEventListener('click', function () {
     chkTab(i);
     showDropDown(i);
 });
-// arrowDown.addEventListener('click', hideDropDown);    
 })
 
 arrowDown.forEach((icon, i) => {
